5be2340d0ed71c81f692ac7fb8f15303b5234934
lingamkalyani <89736226+lingamkalyani@users.noreply.github.com>
2021-09-16 19:50:08 +0530

    Add files via upload

---
+++ inc/activity2.h
@@ -0,0 +1,19 @@
+/**
+ * @file activity2.h
+ * @author 260894 (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-09-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+void InitADC();
+
+uint16_t ReadADC(uint8_t ch);
+
+#endif // ACTIVITY2_H_INCLUDED
